089c86a1158bde18aa0e38caf0f82b7b8db88908
rekey <rekey@me.com>
2017-10-07 07:18:54 +0800

    delete bluebird typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,2 @@
-/// <reference path="../node_modules/@types/node/index.d.ts" />
-/// <reference path="modules/bluebird/index.d.ts" />
 /// <reference path="modules/debug/index.d.ts" />
 /// <reference path="modules/request/index.d.ts" />
